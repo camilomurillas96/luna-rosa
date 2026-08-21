05cd8e55ba07cdeb32498a4774456511e9b088d5
camilomurillas96 <camilogonzalez_96@hotmail.com>
2026-08-20 23:31:07 -0500

    Ajuste CORS y Ajuste responsive

--- backend/src/main/java/com/inventory/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/inventory/backend/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4200", "https://luna-rosa-psi.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4200", "https://luna-rosa-dy.vercel.app"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
